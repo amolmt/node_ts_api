b1320d901ab738eb189454059ce81810999de1a1
Amol Tangade <tangadeamol@gmail.com>
2022-08-07 15:44:36 +0530

    schema validation

---
+++ src/middleware/ValidateSchema.ts
@@ -0,0 +1,42 @@
+import Joi, { ObjectSchema } from 'joi';
+import { Request, Response, NextFunction } from 'express';
+import Logs from '../lib/Logs';
+import { IAuthor } from '../models/Author';
+import { IBook } from '../models/Book';
+
+export const ValidateSchema = (schema: ObjectSchema) => {
+    return async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            await schema.validateAsync(req.body);
+            next();
+        } catch (error) {
+            Logs.error(error);
+            return res.status(422).json({ error });
+        }
+    };
+};
+
+export const Schemas = {
+    author: {
+        create: Joi.object<IAuthor>({
+            name: Joi.string().required()
+        }),
+        update: Joi.object<IAuthor>({
+            name: Joi.string().required()
+        })
+    },
+    book: {
+        create: Joi.object<IBook>({
+            author: Joi.string()
+                .regex(/^[0-9a-fA-F]{24}$/)
+                .required(),
+            title: Joi.string().required()
+        }),
+        update: Joi.object<IBook>({
+            author: Joi.string()
+                .regex(/^[0-9a-fA-F]{24}$/)
+                .required(),
+            title: Joi.string().required()
+        })
+    }
+};
